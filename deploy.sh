161066248dad2153e0269c6ffdee52747d479f5a
Hui-WorkPC <huitingyeong75@gmail.com>
2023-08-21 15:23:51 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -7,20 +7,20 @@ export PATH="$PATH:/usr/src/app/google-cloud-sdk/bin"
 
 gcloud container clusters get-credentials innotech-demo-gke-h --region asia-southeast1 --project innotech-demo | tee -a $logfile
 
-# kubectl create namespace market-demo | tee -a $logfile
+kubectl create namespace market-demo | tee -a $logfile
 
 kubectl apply -f /usr/src/app/app-repo/api-deployment.yaml | tee -a $logfile
 
 sleep 20
 
-# kubectl apply -f api-service.yaml | tee -a $logfile
+kubectl apply -f /usr/src/app/app-repo/api-service.yaml | tee -a $logfile
 
-# sleep 30
+sleep 30
 
-# kubectl apply -f api-ingress.yaml | tee -a $logfile
+kubectl apply -f /usr/src/app/app-repo/api-ingress.yaml | tee -a $logfile
 
-# kubectl apply -f web-deployment.yaml | tee -a $logfile
+kubectl apply -f /usr/src/app/app-repo/web-deployment.yaml | tee -a $logfile
 
-# sleep 20
+sleep 20
 
-# kubectl apply -f web-service.yaml | tee -a $logfile
+kubectl apply -f /usr/src/app/app-repo/web-service.yaml | tee -a $logfile
